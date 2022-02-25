290f2577e380538b635044ae1a2430a7a51b0768
Patoliya <osi.vijay@gmail.com>
2022-02-25 10:39:58 +0530

    Feature - add pushwoosh

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,20 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>Test Pushwoosh Notification!</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <script type="text/javascript" src="https://cdn.pushwoosh.com/webpush/v3/pushwoosh-web-notifications.js" async></script>
+        <script type="text/javascript" src='/pushwoosh.js'></script>
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to my Pushwoosh app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Test Pushwoosh Notification.
+        </p>
+        <p className="description">
+          If you not subscribe then please click on below icon.
         </p>
       </main>
 
---
+++ public/pushwoosh.js
@@ -0,0 +1,22 @@
+var Pushwoosh = Pushwoosh || [];
+Pushwoosh.push(['init', {
+    logLevel: 'info', // possible values: error, info, debug
+    applicationCode: '7E7C1-4903A', // you application code from Pushwoosh Control Panel
+    // safariWebsitePushID: 'web.com.example.domain', //  unique reverse-domain string, obtained in you Apple Developer Portal. Only needed if you send push notifications to Safari browser
+    defaultNotificationTitle: 'Pushwoosh', // sets a default title for push notifications
+    // defaultNotificationImage: 'https://yoursite.com/img/logo-medium.png', // URL to custom custom notification image
+    autoSubscribe: false, // or true. If true, prompts a user to subscribe for pushes upon SDK initialization
+    subscribeWidget: {
+      enable: true
+    },
+    // userId: 'user_id', // optional, set custom user ID
+    // tags: {
+    //     'Name': 'John Smith'   	// optional, set custom Tags
+    // }										
+}]);
+Pushwoosh.push(['onLoad', function (api) {
+  // Executed when a push notification is displayed.
+  Pushwoosh.addEventHandler('receive-push', function (payload) {
+    console.log('Triggered event: receive-push', payload.notification);
+  })
+}])
